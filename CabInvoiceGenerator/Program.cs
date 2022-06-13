5a0c029a2ec6b9098be790b76461509a9ed17cae
mandara11 <mandaram2345@gmail.com>
2022-06-13 14:00:10 +0530

    Add project files.

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Welcome to Cab Invoice Generator");
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+                double fare = invoiceGenerator.CalculateFare(2.0, 5);
+                Console.WriteLine($"Fare : {fare}");
+                Console.ReadLine();
+            }
+        }
+    
+}
+    
