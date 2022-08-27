80fe10a6e81cddcb2da4e8488b48bd42330b6208
mandara11 <mandaram2345@gmail.com>
2022-08-27 22:08:36 +0530

    Added:CabInvoiceGenerator

---
+++ CabInvoiceGenerator/InvoiceSummary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        private int numberofRides;
+        private double totalFare;
+        private double averageFare;
+
+        public InvoiceSummary(int numberofRides, double totalFare)
+        {
+            this.numberofRides=numberofRides;
+            this.totalFare=totalFare;
+            this.averageFare=this.totalFare/this.numberofRides;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is InvoiceSummary)) return false;
+
+            InvoiceSummary inputobject=(InvoiceSummary)obj;
+
+            return this.numberofRides == inputobject.numberofRides && this.totalFare == inputobject.totalFare && this.averageFare == inputobject.averageFare;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.numberofRides.GetHashCode();
+        }
+    }
+}
---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using CabInvoiceGenerator;
+
+namespace CanInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
